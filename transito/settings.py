823b6ffc069578f3dcc8d9b626d730e74739b24f
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-02-13 12:09:40 -0500

    add jwt auth

--- transito/settings.py
+++ transito/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'rest_framework_simplejwt',
     "persona",
     "policia",
     "vehiculo",
@@ -145,3 +147,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
