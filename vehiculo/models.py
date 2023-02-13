92a2eaa872414c874676a5cd44d74d888510c62b
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-02-13 11:58:29 -0500

    add vehiculo class

--- vehiculo/models.py
+++ vehiculo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+from persona.models import Persona
+
+class Vehiculo(models.Model):
+    placa = models.CharField(max_length=10, null=False, blank=False, unique=True)
+    marca = models.CharField(max_length=20, null=False, blank=False)
+    modelo = models.CharField(max_length=20, null=False, blank=False)
+    color = models.CharField(max_length=20, null=False, blank=False)
+    tipo = models.CharField(max_length=20, null=False, blank=False)
+    propietario = models.ForeignKey(Persona, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.placa
