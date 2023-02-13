80bf1a72ca08d56b61814ef2b6bd2fc4cfb6d55c
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-02-13 11:56:38 -0500

    add Policia model

--- policia/models.py
+++ policia/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Policia(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nui = models.CharField(max_length=10, null=False, blank=False, unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Policia.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.policia.save()
--- transito/settings.py
+++ transito/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "persona",
+    "policia",
 ]
 
 MIDDLEWARE = [
