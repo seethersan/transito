9dc7e85503740d88da6ef5be0f7f6163e5889440
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-02-14 13:44:04 -0500

    add swagger doc

--- transito/urls.py
+++ transito/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from rest_framework import permissions
 from rest_framework_simplejwt import views as jwt_views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from rest_framework.routers import DefaultRouter
 
 
@@ -24,12 +27,38 @@ from persona.api.views import PersonaViewSet
 from policia.api.views import RegisterView
 
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Transito API",
+        default_version="v1",
+        description="Docs for Transito API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="carlos_jcez@hotmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 router = DefaultRouter()
 router.register(r"personas", PersonaViewSet, basename="personas")
 router.register(r"vehiculos", VehiculoViewSet, basename="vehiculos")
 router.register(r"infracciones", InfraccionViewSet, basename="infracciones")
 
 urlpatterns = [
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api/register/", RegisterView.as_view(), name="register"),
